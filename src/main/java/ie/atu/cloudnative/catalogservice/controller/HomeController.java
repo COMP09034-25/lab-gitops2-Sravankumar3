a9a8a5a85c679be2b9d1bd4e3938661aa3526cce
Sravankumar3 <sravanamaragonda321@gmail.com>
2026-01-12 18:24:49 +0000

    Fix ECS auto-deploy and update app

--- src/main/java/ie/atu/cloudnative/catalogservice/controller/HomeController.java
+++ src/main/java/ie/atu/cloudnative/catalogservice/controller/HomeController.java
@@ -11,7 +11,7 @@ public class HomeController {
 
     @GetMapping("/")
     public String getGreeting() {
-        return "Welcome to the Cloud Native Book Catalog!";
+        return "Welcome to the Cloud Native Book Catalog! Sravan ";
     }
 
     @GetMapping("/greeting/{name}")
